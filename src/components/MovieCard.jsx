282610dda9fb238c771b36e820cda0d9300aefb9
marcotristani <marco.tristani@hotmail.it>
2026-02-02 21:55:33 +0100

    Milestone 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,11 @@
 import ReactCountryFlag from "react-country-flag";
 import countries from "../assets/countries";
 import { FaFlag } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
+
+const urlImageBase = "https://image.tmdb.org/t/p/";
+const widthImage = "w342";
 
 function MovieCard({ movie }) {
   const {
@@ -10,9 +15,19 @@ function MovieCard({ movie }) {
     vote_average,
     name,
     original_name,
+    poster_path,
   } = movie;
+
+  const vote = Math.ceil(parseFloat(vote_average / 2));
+  const stringStars = [];
+
+  for (let index = 0; index < vote; index++) {
+    stringStars.push(<FaStar key={index} />);
+  }
+
   return (
     <li>
+      <img src={urlImageBase + widthImage + poster_path} alt={title} />
       <h1>{title || name}</h1>
       <h3>{original_title || original_name}</h3>
       <p>
@@ -23,7 +38,7 @@ function MovieCard({ movie }) {
           <FaFlag />
         )}
       </p>
-      <p>{vote_average}</p>
+      <p>{stringStars}</p>
     </li>
   );
 }
